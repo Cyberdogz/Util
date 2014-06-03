4a82809d2ebb4545bca35ccaec25e0653468dcd5
Ren Hoek <zendog42@gmail.com>
2014-06-02 20:31:51 -0500

    Signed-off-by: Ren Hoek <zendog42@gmail.com>

--- App2/src/net/zeroxp/app2/Main.java
+++ App2/src/net/zeroxp/app2/Main.java
@@ -1,5 +1,8 @@
 package net.zeroxp.app2;
 
 public class Main {
+	public static void main(String[] args){
+		
+	}
 
 }
--- JapTime/src/net/zeroxp/japtime/Main.java
+++ JapTime/src/net/zeroxp/japtime/Main.java
@@ -1,5 +1,8 @@
 package net.zeroxp.japtime;
 
 public class Main {
+	public static void main(String[] args) {
+		
+	}
 
 }
